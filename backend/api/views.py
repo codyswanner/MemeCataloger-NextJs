7c3670464814a620022baeda574746fede89433e
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2025-06-22 10:25:38 -0500

    fix(api/views): Remove unused import Caught by ruff linter, the workflow works! 🥳

--- backend/api/views.py
+++ backend/api/views.py
@@ -11,7 +11,7 @@ Classes
 
 """
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
 from rest_framework import generics
 from rest_framework.renderers import JSONRenderer
 from .models import AppUser, Image, Tag, ImageTag
