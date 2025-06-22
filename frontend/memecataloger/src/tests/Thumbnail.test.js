e820ee65dcb8b55ac4c1c24ceb2dae3f95bd699c
Cody Swanner <108250848+codyswanner@users.noreply.github.com>
2025-06-21 19:06:38 -0500

    config(jest): set up Jest Initial setup for Jest unit testing, including an initial test suite to ensure Jest is set up properly. This did require a temporary change to the Thumbnail component, see issue #2 for more details.

---
+++ frontend/memecataloger/src/tests/Thumbnail.test.js
@@ -0,0 +1,42 @@
+import { render, screen } from '@testing-library/react';
+
+import Thumbnail from '../app/Thumbnail';
+
+
+describe('Thumbnail', () => {
+
+  test('renders ImageThumbnail for images', () => {
+    render(
+      <Thumbnail src={'/test-image.jpg'} id={1} />
+    );
+
+    expect(screen.getByRole('presentation')).toBeInTheDocument();
+  });
+
+  test('renders VideoThumbnail for videos', () => {
+    render(
+      <Thumbnail src={'/test-video.mp4'} id={1} />
+    );
+
+    expect(screen.getByTestId('video-thumbnail')).toBeInTheDocument();  
+  });
+
+  test('renders error for unknown filetype', () => {
+    const errorSpy = jest.spyOn(console, 'error').mockImplementation(() => {});
+
+    try {
+      render(
+        <Thumbnail src={'/cow.moo'} id={1} />
+      );
+    } catch (e) {
+      // error expected; do nothing, confirm in asserts
+    };
+
+    expect(errorSpy).toHaveBeenCalledTimes(1);
+    expect(errorSpy).toHaveBeenCalledWith(
+      'Source /cow.moo does not match expected file types'
+    );
+
+    errorSpy.mockRestore();
+  });
+});
