3295550d51e3b50e3fbe3328de37df19ca44272f
codyswanner <108250848+codyswanner@users.noreply.github.com>
2025-08-13 09:41:39 -0500

    Revert "test(frontend): Add Playwright"
    This reverts commit d8c41dead87af0ed1f3481fbe5c8acbb4ef97327.

--- frontend/memecataloger/jest.config.ts
+++ frontend/memecataloger/jest.config.ts
@@ -170,10 +170,9 @@ const jestConfig: Config = {
   // ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-    "/node_modules/",
-    "<rootDir>/tests/"
-  ],
+  // testPathIgnorePatterns: [
+  //   "/node_modules/"
+  // ],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
