21a45f536fd2ecfcd4034a2b0ba4f16fe9917551
codyswanner <108250848+codyswanner@users.noreply.github.com>
2025-08-17 16:14:06 -0500

    try(CI): add test db Django can't start because there's no MySQL, but I'd honestly rather switch the entire project to SQLite than have GitHub actions start up a test version of the MySQL server.  Created a test SQLite DB, and we'll see if that'll cut it 🙂

--- backend/MemeCataloger/settings.py
+++ backend/MemeCataloger/settings.py
@@ -103,11 +103,11 @@ DATABASES = {
 # https://stackoverflow.com/questions/4650509/different-db-for-testing-in-django
 def is_test_env():
     return os.getenv("TEST", False)
-
+    
 if 'test' in sys.argv or is_test_env():
     DATABASES['default'] = {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase'
+        'NAME': str(Path(f'{BASE_DIR}/test_db/test.db'))
     }
 
 
