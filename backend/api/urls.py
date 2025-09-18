6f41b8ffe7eee5ff6f271313f3ca4ac5f96dfba3
codyswanner <108250848+codyswanner@users.noreply.github.com>
2025-09-18 17:26:21 -0500

    clean(urls): create chunks for easier viewing of related URLs

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -17,11 +17,14 @@ app_name = 'api'
 urlpatterns = [
     path('user/', AppUserListView.as_view()),
     path('user/<uuid:user_id>', user_view),
+    
     path('image/', ImageListView.as_view()),
     path('image/<uuid:image_id>', image_view),
+
     path('tag/', TagListView.as_view()),
     path('tag/<uuid:tag_id>', existing_tag_view),
     path('tag/new', new_tag_view),
+
     path('image-tag/', ImageTagListView.as_view()),
     path('image-tag/<uuid:imagetag_id>', existing_imagetag_view),
     path('image-tag/new', new_imagetag_view)
